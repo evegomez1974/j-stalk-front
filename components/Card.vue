2119ba5775df1801fc8476fc44d2d7ed928b35a1
Eve.gomez <eve.gomez@campus.academy>
2023-01-31 21:57:04 +0100

    JS-43: Implémentation éléments card

---
+++ components/Card.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="card">
+    <h5 class="card-title">Developpeur cpp junior H/F</h5>
+    <div class="card-body">
+      <h6 class="card-subtitle mb-2 text-muted">PellencST</h6>
+      <h6 class="card-subtitle mb-2 text-muted">Pertuis, Vaucluse(84)</h6>
+      <p class="card-text">
+        <BIconCircleFill
+          style="color: black"
+          font-scale="0.5"
+        ></BIconCircleFill>
+        Développer le front-end des applications web en mode agile
+      </p>
+      <p class="card-text">
+        <BIconCircleFill style="color: black" font-scale="0.5">
+        </BIconCircleFill> Optimiser l'expérience utilisateur
+      </p>
+      <a href="#" class="card-link">Voir plus</a>
+    </div>
+  </div>
+</template>
+
+<script>
+import { BIconCircleFill } from "bootstrap-vue";
+export default {
+  name: "Card",
+  components: {
+    BIconCircleFill,
+  },
+};
+</script>
---
+++ pages/test.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="test">
+      <Card />
+      <Card />
+  </div>
+</template>
+
+<script>
+import "../assets/css/card.scss";
+import "../assets/css/themes.scss";
+import Card from "../components/Card.vue";
+export default {
+  name: "name",
+  components: { Card },
+};
+</script>
+
+<style lang="scss">
+.test {
+  display: flex;
+  justify-content: space-evenly;
+}
+
+@media only screen and (max-width: 1000px) {
+  .test {
+    height: 100vh;
+    flex-direction: column;
+    align-items: center;
+  }
+}
+</style>
