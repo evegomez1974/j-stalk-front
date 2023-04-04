a1d9e253f9b5dc8444fcdf27aafa5a207a20256c
leagrave <lea.grave@orange.fr>
2023-04-04 11:28:52 +0200

    premier test API sur Login

--- components/formLogin.vue
+++ components/formLogin.vue
@@ -2,17 +2,17 @@
   <form @submit.prevent="submitForm">
       <div class="containerLog">
         <div>
-          <input class="inputLog" type="email" v-model="FormData.email" placeholder="Email">
+          <input class="inputLog" type="email" v-model="email" placeholder="Email">
         </div>
         <div>
-          <input class="inputLog" type="password" v-model="FormData.password" placeholder="Mot de passe">
+          <input class="inputLog" type="password" v-model="password" placeholder="Mot de passe">
         </div>
         <div class="lien">
           <button @click="methVal"><label class="labelOublie">Mot de passe oublié ?</label></button>
         </div>
         <div class="btnRouter">
           <div class="btnVal">
-            <router-link to="/"><button type="submit" >Se connecter</button></router-link>
+            <button type="submit" @click="login">Se connecter</button>
           </div>
           <div class="btnVal">
             <router-link to="/"><button type="submit">Inscription</button></router-link>
@@ -30,26 +30,46 @@ export default {
   components: {},
   data (){
     return {
-    FormData: {
       email:'',
       password: '',
-    },
+
     myValueOublieMotDePasse: "oublieMotDePasse"
   }
   },
   methods: {
-    async submitForm() {
-      try {
-        const response = await axios.post('http://localhost:3000', this.formData)
-        console.log(response.data)
-      } catch (error) {
-        console.error(error)
-      }
-    },
+    // async submitForm() {
+    //   try {
+    //     const response = await axios.post('http://localhost:3000', this.formData)
+    //     console.log(response.data)
+    //   } catch (error) {
+    //     console.error(error)
+    //   }
+    // },
     methVal() {
       this.$emit('message-sent', this.myValueOublieMotDePasse);
     }
-  }
+  },
+  login() {
+      fetch('http://127.0.0.1:3000/users/login', {
+        headers: {
+          'Authorization': 'Basic ' + btoa(`${this.email}:${this.password}`)
+        },
+        method: 'get',
+      })
+      .then(res => {
+        if (res.status != "200") {
+          this.error = "Une erreur est survenue, veuillez réessayer";
+        }
+        else {
+          return res.json()
+        }
+      })
+      .then(data => {
+        localStorage.setItem('PAC-token', data.token);
+        console.log(data.token)
+        router.push('/');
+      })
+    }
 
 }
 </script>
