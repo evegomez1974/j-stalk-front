4483225f9e95f90513d460832ab3756ec1f0e21c
leagrave <lea.grave@orange.fr>
2023-05-12 14:16:12 +0200

    ajout du formulaire d'inscription

--- components/formSignUp.vue
+++ components/formSignUp.vue
@@ -2,11 +2,22 @@
   <form @submit.prevent="login">
       <div class="containerLog">
         <h2>S'inscrire</h2>
+        <div class="selected">
+          <div>Selected: {{ selectedItem }}</div>
+
+          <select name="choice" v-model="selectedItem" @change="onSelectChange()">
+            <option disabled value="">Please select one</option>
+            <option value="ecole" selected>Ecole</option>
+            <option value="entreprise">Entreprise</option>
+            <option value="etudiant">Etudiant</option>
+            <option value="intervenant">Intervenant</option>
+          </select>
+        </div>
         <div>
           <input class="inputLog" type="text" v-model="name" placeholder="Nom">
         </div>
         <div>
-          <input class="inputLog" type="text" v-model="firstname" placeholder="Prénom">
+          <input class="inputLog" type="text" v-model="firstname" placeholder="Prénom" v-if="isVisibleEcole">
         </div>
         <div>
           <input class="inputLog" type="number" v-model="phoneNumber" placeholder="Téléphone">
@@ -17,9 +28,27 @@
         <div>
           <input class="inputLog" type="password" v-model="password" placeholder="Mot de passe">
         </div>
+
         <div>
-          <input class="inputLog" type="img" v-model="pictures" placeholder="Image">
+          <div v-if="isVisibleIntervenant">
+            <input class="inputLog" type="text" v-model="nomEcole" placeholder="Ecole">
+          </div>
+          <div>
+            <select name="choice" v-model="contratType" v-if="isVisibleEtudiant">
+              <option value="Stage">Stage</option>
+              <option value="apprentissage" selected>Apprentissage</option>
+              <option value="professionnalisation">Professionnalisation</option>
+            </select>
+          </div>
+          <div>
+            <select name="choice" v-model="postType" placeholder="Type de post" v-if="isVisibleEtudiant">
+              <option value="dev">Developpeur</option>
+              <option value="market" selected>Marketing</option>
+              <option value="data">DATA IA</option>
+            </select>
+          </div>
         </div>
+
         <div class="btnRouter">
           <div class="btnVal">
             <router-link to="/Login"><button type="submit">Annuler</button></router-link>
@@ -46,6 +75,10 @@ export default {
       firstname: '',
       pictures: '',
       phoneNumber: '',
+      selectedItem: '',
+      isVisibleEtudiant: false,
+      isVisibleEcole: true,
+      isVisibleIntervenant: false,
   }
   },
   methods: {
@@ -79,16 +112,37 @@ export default {
                 localStorage.setItem('PAC-token', data.token);
                 router.push('/Login');
             })
-        }
+        },
+        onSelectChange() {
+      // Mettre à jour la propriété selectedItem ici
+          console.log("selectedItem:", this.selectedItem);
+          if (this.selectedItem === "etudiant") {
+            this.isVisibleEtudiant = !this.isVisibleEtudiant;
+            this.isVisibleIntervenant = !this.isVisibleIntervenant;
+          }
+          if (this.selectedItem === "ecole") {
+            this.isVisibleEcole = !this.isVisibleEcole;
+          }
+          if (this.selectedItem === "entreprise") {
+            this.isVisibleEcole = !this.isVisibleEcole;
+          }
+          if (this.selectedItem === "intervenant") {
+            this.isVisibleIntervenant = !this.isVisibleIntervenant;
+          }
+    }
 
-}
   }
+}
 
 </script>
 
 
-<style>
+<style scoped>
 
+select{
+  border-radius: 5px;
+  padding: 5px;
+}
 
 div.btnVal {
   width: 17%;
