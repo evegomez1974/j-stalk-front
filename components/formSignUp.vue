62c281f0a927597101ea49be86221383ed3469ca
Eve.gomez <eve.gomez@campus.academy>
2023-05-14 22:44:50 +0200

    ajout formulaire inscription user + étudiants

--- components/formSignUp.vue
+++ components/formSignUp.vue
@@ -1,145 +1,234 @@
 <template>
-  <form @submit.prevent="login">
-      <div class="containerLog">
-        <h2>S'inscrire</h2>
-        <div class="selected">
-          <div>Selected: {{ selectedItem }}</div>
-
-          <select name="choice" v-model="selectedItem" @change="onSelectChange()">
-            <option disabled value="">Please select one</option>
-            <option value="ecole" selected>Ecole</option>
-            <option value="entreprise">Entreprise</option>
-            <option value="etudiant">Etudiant</option>
-            <option value="intervenant">Intervenant</option>
-          </select>
+  <form>
+    <div class="containerLog">
+      <h2>S'inscrire</h2>
+      <div class="selected">
+        <select name="userStatus" v-model="formUser.userStatus" required>
+          <option disabled value="" selected>Please select one</option>
+          <option value="Etudiant">Etudiant</option>
+          <option value="Entreprise">Entreprise</option>
+          <option value="Ecole">Ecole</option>
+          <option value="Intervenant">Intervenant</option>
+        </select>
+      </div>
+      <div class="formUser">
+        <div>
+          <input
+            class="inputLog"
+            type="text"
+            v-model="formUser.name"
+            placeholder="Nom"
+            required
+          />
         </div>
         <div>
-          <input class="inputLog" type="text" v-model="name" placeholder="Nom">
+          <input
+            class="inputLog"
+            type="text"
+            v-model="formUser.firstname"
+            placeholder="Prénom"
+          />
         </div>
         <div>
-          <input class="inputLog" type="text" v-model="firstname" placeholder="Prénom" v-if="isVisibleEcole">
+          <input
+            class="inputLog"
+            type="number"
+            v-model="formUser.phoneNumber"
+            placeholder="Numero de téléphone"
+            required
+          />
         </div>
         <div>
-          <input class="inputLog" type="number" v-model="phoneNumber" placeholder="Téléphone">
+          <input
+            class="inputLog"
+            type="email"
+            v-model="formUser.email"
+            placeholder="Email"
+          />
         </div>
         <div>
-          <input class="inputLog" type="email" v-model="email" placeholder="Email">
-        </div>
-        <div>
-          <input class="inputLog" type="password" v-model="password" placeholder="Mot de passe">
-        </div>
-
-        <div>
-          <div v-if="isVisibleIntervenant">
-            <input class="inputLog" type="text" v-model="nomEcole" placeholder="Ecole">
-          </div>
-          <div>
-            <select name="choice" v-model="contratType" v-if="isVisibleEtudiant">
-              <option value="Stage">Stage</option>
-              <option value="apprentissage" selected>Apprentissage</option>
-              <option value="professionnalisation">Professionnalisation</option>
-            </select>
-          </div>
-          <div>
-            <select name="choice" v-model="postType" placeholder="Type de post" v-if="isVisibleEtudiant">
-              <option value="dev">Developpeur</option>
-              <option value="market" selected>Marketing</option>
-              <option value="data">DATA IA</option>
-            </select>
-          </div>
+          <input
+            class="inputLog"
+            type="password"
+            v-model="formUser.password"
+            placeholder="Mot de passe"
+            required
+          />
         </div>
 
         <div class="btnRouter">
           <div class="btnVal">
-            <router-link to="/Login"><button type="submit">Annuler</button></router-link>
+            <router-link to="/Login"
+              ><button type="submit">Annuler</button></router-link
+            >
           </div>
           <div class="btnVal">
-            <button type="submit" @click="login">Inscription</button>
+            <button type="submit">Inscription</button>
           </div>
         </div>
+      </div>
+      <div v-if="formUser.userStatus === 'Etudiant'" class="formStudent">
+        <div>
+          <select name="jobType" v-model="formStudent.jobType" required>
+            <option disabled value="" selected>Please select one</option>
+            <option value="Alternance">Alternance</option>
+            <option value="Stage">Stage</option>
+          </select>
+        </div>
+        <div>
+          <select v-if="formStudent.jobType === 'Alternance'" name="jobType" v-model="formStudent.contractType" required>
+            <option disabled value="" selected>Please select one</option>
+            <option value="Alternance">Apprentissage</option>
+            <option value="Stage">Professionalisation</option>
+          </select>
+        </div>
+        <div>
+          <select v-if="formStudent.jobType === 'Alternance'" name="jobType" v-model="formStudent.contractLength" required>
+            <option disabled value="" selected>Please select one</option>
+            <option value="1 an">1 an</option>
+            <option value="2 ans">2 ans</option>
+            <option value="3 ans">3 ans</option>
+          </select>
+          <select v-if="formStudent.jobType === 'Stage'" name="jobType" v-model="formStudent.contractLength" required>
+            <option disabled value="" selected>Please select one</option>
+            <option value="3 mois">3 mois</option>
+            <option value="6 mois">6 mois</option>
+            <option value="9 mois">9 mois</option>
+            <option value="1 an">1 an</option>
+          </select>
+        </div>
+        <div>
+          <select name="jobType" v-model="formStudent.yearSchool" required>
+            <option disabled value="" selected>Please select one</option>
+            <option value="1ère année">1ère année</option>
+            <option value="2ème année">2ème année</option>
+            <option value="3ème année">3ème année</option>
+          </select>
+          <select name="jobType" v-model="formStudent.typeDegree" required>
+            <option disabled value="" selected>Please select one</option>
+            <option value="License">License</option>
+            <option value="License professionnelle">License professionnelle</option>
+            <option value="Bachelor">Bachelor</option>
+            <option value="Master">Master</option>
+            <option value="Master professionnel">Master professionnel</option>
+            <option value="BTS">BTS</option>
+            <option value="DUT">DUT</option>
+            <option value="IUT">IUT</option>
+            <option value="CAP">CAP</option>
+            <option value="BEP">BEP</option>
+          </select>
+        </div>
+        <div>
+          <input
+            class="inputLog"
+            type="text"
+            v-model="formStudent.nameSchool"
+            placeholder="Nom de l'école"
+            required
+          />
+        </div>
+        <div>
+          <textarea
+            name="message"
+            v-model="formStudent.description"
+            rows="4"
+            cols="40"
+            maxLength="200"
+            placeholder="Saisissez votre message ici"
+          ></textarea>
+        </div>
+      </div>
     </div>
   </form>
 </template>
 
 <script>
-
-
 export default {
-  name: 'FormSignUp',
+  name: "FormSignUp",
   components: {},
-  data (){
+  data() {
     return {
-      email:'',
-      password: '',
-      name: '',
-      firstname: '',
-      pictures: '',
-      phoneNumber: '',
-      selectedItem: '',
-      isVisibleEtudiant: false,
-      isVisibleEcole: true,
-      isVisibleIntervenant: false,
-  }
+      formUser: {
+        email: null,
+        password: null,
+        name: null,
+        firstname: null,
+        phoneNumber: null,
+        pictures: null,
+        userStatus: "",
+      },
+      formStudent: {
+        jobType: "",
+        contractType: "",
+        contractLength: "",
+        yearSchool: "",
+        typeDegree: "",
+        nameSchool: null,
+        description: null,
+      },
+      // selectedItem: null,
+      // isVisibleEtudiant: false,
+      // isVisibleEcole: true,
+      // isVisibleIntervenant: false,
+    };
   },
-  methods: {
-    signUp () {
-            if (this.password != this.password1) {
-                this.error = "Vos mots de passe ne correspondent pas";
-                return;
-            }
-            const bodyFormData = new FormData();
-            bodyFormData.append('name', this.name);
-            bodyFormData.append('firstname', this.firstname);
-            bodyFormData.append('email', this.email);
-            bodyFormData.append('phoneNumber', this.phoneNumber);
-            bodyFormData.append('password', this.password);
-            bodyFormData.append('pictures', this.pictures);
-            fetch('http://127.0.0.1:8080/users/signup', {
-                body: bodyFormData,
-                method: 'post',
-            })
-            .then(res => {
-                // console.log(res);
-                if(res.status != 200) {
-                    this.error = "Une erreur est survenue, veuillez réessayer";
-                }
-                else {
-                    return res.json();
-                }
-            })
-            .then(data => {
-                // console.log("data:", data);
-                localStorage.setItem('PAC-token', data.token);
-                router.push('/Login');
-            })
-        },
-        onSelectChange() {
-      // Mettre à jour la propriété selectedItem ici
-          console.log("selectedItem:", this.selectedItem);
-          if (this.selectedItem === "etudiant") {
-            this.isVisibleEtudiant = !this.isVisibleEtudiant;
-            this.isVisibleIntervenant = !this.isVisibleIntervenant;
-          }
-          if (this.selectedItem === "ecole") {
-            this.isVisibleEcole = !this.isVisibleEcole;
-          }
-          if (this.selectedItem === "entreprise") {
-            this.isVisibleEcole = !this.isVisibleEcole;
-          }
-          if (this.selectedItem === "intervenant") {
-            this.isVisibleIntervenant = !this.isVisibleIntervenant;
-          }
-    }
+  //   methods: {
+  //     signUp () {
+  //             if (this.password != this.password1) {
+  //                 this.error = "Vos mots de passe ne correspondent pas";
+  //                 return;
+  //             }
+  //             const bodyFormData = new FormData();
+  //             bodyFormData.append('name', this.name);
+  //             bodyFormData.append('firstname', this.firstname);
+  //             bodyFormData.append('email', this.email);
+  //             bodyFormData.append('phoneNumber', this.phoneNumber);
+  //             bodyFormData.append('password', this.password);
+  //             bodyFormData.append('pictures', this.pictures);
+  //             fetch('http://127.0.0.1:8080/users/signup', {
+  //                 body: bodyFormData,
+  //                 method: 'post',
+  //             })
+  //             .then(res => {
+  //                 // console.log(res);
+  //                 if(res.status != 200) {
+  //                     this.error = "Une erreur est survenue, veuillez réessayer";
+  //                 }
+  //                 else {
+  //                     return res.json();
+  //                 }
+  //             })
+  //             .then(data => {
+  //                 // console.log("data:", data);
+  //                 localStorage.setItem('PAC-token', data.token);
+  //                 router.push('/Login');
+  //             })
+  //         },
+  //         onSelectChange() {
+  //       // Mettre à jour la propriété selectedItem ici
+  //           console.log("selectedItem:", this.selectedItem);
+  //           if (this.selectedItem === "etudiant") {
+  //             this.isVisibleEtudiant = !this.isVisibleEtudiant;
+  //             this.isVisibleIntervenant = !this.isVisibleIntervenant;
+  //           }
+  //           if (this.selectedItem === "ecole") {
+  //             this.isVisibleEcole = !this.isVisibleEcole;
+  //           }
+  //           if (this.selectedItem === "entreprise") {
+  //             this.isVisibleEcole = !this.isVisibleEcole;
+  //           }
+  //           if (this.selectedItem === "intervenant") {
+  //             this.isVisibleIntervenant = !this.isVisibleIntervenant;
+  //           }
+  //     }
 
-  }
-}
-
+  //   }
+};
 </script>
 
 
 <style scoped>
-
-select{
+select {
   border-radius: 5px;
   padding: 5px;
 }
@@ -148,9 +237,8 @@ div.btnVal {
   width: 17%;
   display: inline-block;
   border-radius: 10px;
-  border-color: #4e4a55 ;
-  background-color: #b298dc ;
+  border-color: #4e4a55;
+  background-color: #b298dc;
   margin-inline: 20px;
 }
-
 </style>
--- pages/SignUp.vue
+++ pages/SignUp.vue
@@ -2,7 +2,7 @@
   <div id="auth">
     <img id="titleLog" src="../static/j-stalk-logo-texte.png">
     <div>
-      <formSignUp @message-sent="updateMessage" />
+      <FormSignUp/>
     </div>
   </div>
 </template>
@@ -12,12 +12,12 @@
 import { defineComponent } from 'vue';
 import '../assets/css/themes.scss'
 import  '../assets/css/Login.scss'
-import formSignUp from '../components/formSignUp'
+import FormSignUp from '../components/formSignUp'
 
 
 export default defineComponent({
   name: 'signup',
-  components: { formSignUp },
+  components: { FormSignUp },
   data (){
     return {
       message:"erreur",
