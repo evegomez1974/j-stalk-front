3f51c2e7bb26e56c4d36ef89b050e5b0e43d0bba
Eve.gomez <eve.gomez@campus.academy>
2023-05-16 17:22:50 +0200

    positionnement en grille

--- components/infosUserProfil.vue
+++ components/infosUserProfil.vue
@@ -1,173 +1,359 @@
 <template>
-  <div>
-    <div>
-      <div class="mt-4">
-        <b-card>
-          <img :src="base64Image" alt="Image" img-left  id="img-user" width="150" height="200">
+  <div class="pageContainer">
+    <!-- <div class="grid2rows">
+      <div class="row1">
+        <div class="column1"></div>
+        <div class="column2">
+          <div class="subRow1"></div>
+          <div class="subRow2">
+            <div class="subItem"></div>
+            <div class="subItem"></div>
+            <div class="subItem"></div>
+            <div class="subItem"></div>
+          </div>
+        </div>
+      </div>
+      <div class="row2"></div>
+    </div> -->
+
+    <!-- __________________________________ -->
+
+    <div class="grid2rows">
+      <div class="row1">
+        <div class="column1">
+          <div class="profilPicture">
+    <img
+      :src="base64Image"
+      alt="Image"
+      img-left
+      id="img-user"
+      style="max-width: 100%; max-height: 100%; object-fit: contain;"
+    />
+          </div>
           <div id="ajout_de_photo">
             <div id="separateur_photo">
-                <label for="image">
-                  <img id="add_photo_logo"
+              <label for="image">
+                <img
+                  id="add_photo_logo"
                   src="../assets/img/add-camera.png"
-                  alt="appareil photo">
-                </label>
-                  <input id="image" v-on:change="onFileChange" type="file" class="image">
+                  alt="appareil photo"
+                />
+              </label>
+              <input
+                id="image"
+                v-on:change="onFileChange"
+                type="file"
+                class="image"
+              />
+            </div>
+          </div>
+        </div>
+        <div class="column2">
+          <div class="subRow1">
+            <h4>{{ userInfo.firstName }} {{ userInfo.name }}</h4>
+          </div>
+          <div class="subRow2">
+            <div class="subItem">
+              <b-badge class="badge" variant="light">{{
+                userInfo.yearSchool
+              }}</b-badge>
+            </div>
+            <div class="subItem">
+              <b-badge class="badge" variant="light">{{
+                userInfo.nameSchool
+              }}</b-badge>
+            </div>
+            <div class="subItem">
+              <b-badge class="badge" variant="light">{{
+                userInfo.jobType
+              }}</b-badge>
+            </div>
+            <div class="subItem">
+              <b-badge class="badge" variant="light">{{
+                userInfo.contractType
+              }}</b-badge>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row2">
+        <b-badge class="badge" variant="light">{{
+          userInfo.description
+        }}</b-badge>
+      </div>
+    </div>
+
+    <!-- <div>
+      <div class="mt-4">
+        <b-card>
+          <img
+            :src="base64Image"
+            alt="Image"
+            img-left
+            id="img-user"
+            width="150"
+            height="200"
+          />
+          <div id="ajout_de_photo">
+            <div id="separateur_photo">
+              <label for="image">
+                <img
+                  id="add_photo_logo"
+                  src="../assets/img/add-camera.png"
+                  alt="appareil photo"
+                />
+              </label>
+              <input
+                id="image"
+                v-on:change="onFileChange"
+                type="file"
+                class="image"
+              />
             </div>
           </div>
 
-          <h4>{{ userInfo.firstName}} {{ userInfo.name }}</h4>
+          <h4>{{ userInfo.firstName }} {{ userInfo.name }}</h4>
           <b-card-text class="zoneInfosUser">
             <div class="zoneUser">
               <b-label>{{ userInfo.nameSchool }}</b-label>
             </div>
-            <div  class="zoneUser">
+            <div class="zoneUser">
               <b-label>{{ userInfo.contractType }}</b-label>
             </div>
-            <div  class="zoneUser">
+            <div class="zoneUser">
               <b-label>{{ userInfo.jobType }}</b-label>
             </div>
-            <div  class="zoneUser">
+            <div class="zoneUser">
               <b-label>{{ userInfo.yearSchool }}</b-label>
             </div>
           </b-card-text>
           <div>
-              <b-label>{{ userInfo.description }}</b-label>
+            <b-label>{{ userInfo.description }}</b-label>
           </div>
-
-
         </b-card>
       </div>
-    </div>
+    </div> -->
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'infosUserProfil',
+  name: "infosUserProfil",
   components: {},
   props: {
-      userInfo: Object,
-
-    },
-  data (){
+    userInfo: Object,
+  },
+  data() {
     return {
       busy: false,
-        processing: false,
-        counter: 1,
-        interval: null,
-        image: document.getElementById("image"),
-        base64Image: this.userInfo.pictures,
-        base64:"",
-
-
-  }
+      processing: false,
+      counter: 1,
+      interval: null,
+      image: document.getElementById("image"),
+      base64Image: this.userInfo.pictures,
+      base64: "",
+    };
   },
   beforeDestroy() {
-      this.clearInterval()
-    },
+    this.clearInterval();
+  },
   watch: {
     image: function (newVal, oldVal) {
-      if(newVal) {
-        console.log(newVal)
+      if (newVal) {
+        console.log(newVal);
         this.createBase64Image(newVal);
       } else {
         this.base64 = null;
       }
-    }
+    },
   },
   methods: {
-    createBase64Image: function(FileObject) {
+    createBase64Image: function (FileObject) {
       const reader = new FileReader();
       reader.onload = (event) => {
         this.base64 = event.target.result;
-        console.log(this.base64)
+        console.log(this.base64);
         const bodyFormData = new FormData();
-        bodyFormData.append('pictures', this.base64);
-        fetch('http://127.0.0.1:8080/userPictures' , {
+        bodyFormData.append("pictures", this.base64);
+        fetch("http://127.0.0.1:8080/userPictures", {
           body: bodyFormData,
-          method: 'post',
+          method: "post",
           headers: {
-            'Authorization': `Bearer ${localStorage.getItem('PAC-token')}`
+            Authorization: `Bearer ${localStorage.getItem("PAC-token")}`,
           },
         })
-        .then(res => {
-          // console.log(res);
-          if(res.status != 200) {
-            this.error = "Une erreur est survenue, veuillez réessayer";
-          }
-          else {
-            return res.json();
-
-          }
-        })
-        .then(res => {
-          console.log(res.data);
-
+          .then((res) => {
+            // console.log(res);
+            if (res.status != 200) {
+              this.error = "Une erreur est survenue, veuillez réessayer";
+            } else {
+              return res.json();
+            }
           })
-        };
-        reader.readAsDataURL(FileObject);
-      },
+          .then((res) => {
+            console.log(res.data);
+          });
+      };
+      reader.readAsDataURL(FileObject);
+    },
     clearInterval() {
-        if (this.interval) {
-          clearInterval(this.interval)
-          this.interval = null
+      if (this.interval) {
+        clearInterval(this.interval);
+        this.interval = null;
+      }
+    },
+    onShown() {
+      // Focus the dialog prompt
+      this.$refs.dialog.focus();
+    },
+    onHidden() {
+      // In this case, we return focus to the submit button
+      // You may need to alter this based on your application requirements
+      this.$refs.submit.focus();
+    },
+    onSubmit() {
+      this.processing = false;
+      this.busy = true;
+    },
+    onCancel() {
+      this.busy = false;
+    },
+    onOK() {
+      this.counter = 1;
+      this.processing = true;
+      // Simulate an async request
+      this.clearInterval();
+      this.interval = setInterval(() => {
+        if (this.counter < 20) {
+          this.counter = this.counter + 1;
+        } else {
+          this.clearInterval();
+          this.$nextTick(() => {
+            this.busy = this.processing = false;
+          });
         }
-      },
-      onShown() {
-        // Focus the dialog prompt
-        this.$refs.dialog.focus()
-      },
-      onHidden() {
-        // In this case, we return focus to the submit button
-        // You may need to alter this based on your application requirements
-        this.$refs.submit.focus()
-      },
-      onSubmit() {
-        this.processing = false
-        this.busy = true
-      },
-      onCancel() {
-        this.busy = false
-      },
-      onOK() {
-        this.counter = 1
-        this.processing = true
-        // Simulate an async request
-        this.clearInterval()
-        this.interval = setInterval(() => {
-          if (this.counter < 20) {
-            this.counter = this.counter + 1
-          } else {
-            this.clearInterval()
-            this.$nextTick(() => {
-              this.busy = this.processing = false
-            })
-          }
-        }, 350)
-      },
-      onFileChange(e) {
-        const files = e.target.files;
+      }, 350);
+    },
+    onFileChange(e) {
+      const files = e.target.files;
       if (files.length > 0) {
         this.image = files[0];
       } else {
-        this.image = null
+        this.image = null;
       }
-
     },
-
   },
-
-
-
-
-}
+};
 </script>
 
 
 <style scoped>
+.pageContainer {
+  height: 100%;
+  width: 100%;
+}
 
-.zoneInfosUser {
+.grid2rows {
+  height: 100%;
+  width: 100%;
+  display: grid;
+  grid-template-rows: 1.5fr 0.5fr;
+  grid-gap: 10px;
+}
+
+.row1 {
+  height: 100%;
+  display: grid;
+  grid-template-columns: 0.5fr 1fr;
+  grid-gap: 10px;
+  background: #1321a3;
+  padding: 10px;
+}
+
+.row2 {
+  background: #1321a3;
+  padding: 10px;
+}
+
+.column1 {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  max-width: 100%;
+  background: #13a354;
+}
+
+.column2 {
+  height: 100%;
+  display: grid;
+  grid-template-rows: 0.5fr 1.5fr;
+  grid-gap: 10px;
+  background-color: #13a354;
+}
+
+.subRow1 {
+  padding-left: 10px;
+  padding-right: 10px;
+  padding-top: 20px;
+  padding-bottom: 0px;
+  background-color: #a3135b;
+}
+
+.subRow2 {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr 1fr;
+  grid-gap: 10px;
+  padding: 10px;
+  background-color: #a3135b;
+}
+.subItem {
+  /*  border-top: 10px solid var(--jstalk-color-primary);*/
+  border-radius: 4%;
+  /* padding: 10px;*/
+  background-color: #c42d12;
+}
+
+.badge {
+  font-size: 1em;
+  font-weight: 400;
+  text-align: left;
+  padding-left: 5%;
+  padding-bottom: 0;
+  height: 100%;
+  width: 100%;
+  border-top: 10px solid var(--jstalk-color-primary);
+}
+
+h4 {
+  font-weight: 700;
+}
+
+.profilPicture {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  /*width: 200px; /* Taille fixe du rectangle */
+ /* height: 200px; /* Taille fixe du rectangle */
+  background-color: #ccc; /* Couleur de fond du rectangle */
+  margin-bottom: 4%;
+}
+
+#ajout_de_photo {
+  background: peru;
+}
+
+/*img {
+  height: 20%;
+  width: 100%;
+}
+*/
+
+/*__________________________________________*/
+
+/*.zoneInfosUser {
   display: grid;
   grid-template-columns: repeat(2, auto);
 }
@@ -176,7 +362,6 @@ export default {
   border: 1px solid black;
   margin: 10px;
 }
-
 
 .image {
   display: none;
@@ -197,7 +382,6 @@ export default {
   align-items: center;
   padding: 0 3px;
 }
-
 
 #separateur_photo {
   width: 100%;
@@ -235,8 +419,5 @@ export default {
   height: min-content;
   display: flex;
   flex-direction: column;
-}
-
-
-
+}*/
 </style>
--- pages/profil.vue
+++ pages/profil.vue
@@ -1,112 +1,179 @@
 <template>
   <div>
+    <!-- <div id="app">
+      <div class="grid">
+        <div class="left-column">
+          <div class="row-1">hello</div>
+          <div class="row-2">
+            <div class="grid-row-2">
+              <div class="grid-row-2-item">Item 1</div>
+              <div class="grid-row-2-item">Item 2</div>
+              <div class="grid-row-2-item">Item 3</div>
+              <div class="grid-row-2-item">Item 4</div>
+              <div class="grid-row-2-item">Item 5</div>
+            </div>
+          </div>
+        </div>
+        <div class="right-column">texto</div>
+      </div>
+    </div> -->
+
+    <!-- __________________________________ -->
+
     <div>
-      <NavBar/>
+      <NavBar />
     </div>
 
-      <div class="listCards">
-
-        <div class="cardUser">
-          <div class="infosUserProfil">
-            <infosUserProfil v-for="userInfo in listUserInfos" :key="userInfo.id" :userInfo="userInfo" />
+    <div id="app">
+      <div class="grid">
+        <div class="left-column">
+          <div class="row-1">
+            <infosUserProfil
+              v-for="userInfo in listUserInfos"
+              :key="userInfo.id"
+              :userInfo="userInfo"
+            />
+          </div>
+          <div class="row-2">
+            <div class="grid-row-2">
+              <listDocsUser
+                class="grid-row-2-item"
+                v-for="userDocs in listUserDocs"
+                :key="userDocs.id"
+                :userDocs="userDocs"
+                @message-sent="updateMessage"
+              />
+            </div>
           </div>
         </div>
-
-        <div class="cardUser">
-          <div class="contact">
-            <contact v-for="userInfo in listUserInfos" :key="userInfo.id" :userInfo="userInfo"/>
-          </div>
-        </div>
-
-        <div class="cardUser">
+        <div class="right-column">
           <div class="container" v-if="userPDF[0]">
-            <pdfViewer  :pdf="userPDF[0]"
-            >
-            </pdfViewer>
+            <pdfViewer :pdf="userPDF[0]"> </pdfViewer>
           </div>
         </div>
+      </div>
+    </div>
 
-        <div class="cardUser">
-          <div class="btnAddDocs" v-if="isVisibleBtnAddDoc">
-            <button @click="btnAddDoc">+</button>
-          </div>
+
+
+    <!-- ______________________________________ -->
+
+    <!-- fait -->
+    <!-- <div class="listCards">
+      <div class="cardUser">
+        <div class="infosUserProfil">
+          <infosUserProfil
+            v-for="userInfo in listUserInfos"
+            :key="userInfo.id"
+            :userInfo="userInfo"
+          />
         </div>
+      </div>
 
-        <div class="cardUser">
-          <div class="addDocs" v-if="isVisibleAddDoc">
-            <addDocsVue  @message-sent-pdf="updateMessagePdf"/>
-          </div>
+
+      <div class="cardUser">
+        <div class="contact">
+          <contact
+            v-for="userInfo in listUserInfos"
+            :key="userInfo.id"
+            :userInfo="userInfo"
+          />
         </div>
+      </div> -->
 
-        <div class="cardUser">
-          <div class="listDocsUser">
-            <listDocsUser v-for="userDocs in listUserDocs" :key="userDocs.id" :userDocs="userDocs"  @message-sent="updateMessage"/>
-          </div>
+<!-- fait -->
+      <!-- <div class="cardUser">
+        <div class="container" v-if="userPDF[0]">
+          <pdfViewer :pdf="userPDF[0]"> </pdfViewer>
         </div>
+      </div>
 
-     </div>
+      <div class="cardUser">
+        <div class="btnAddDocs" v-if="isVisibleBtnAddDoc">
+          <button @click="btnAddDoc">+</button>
+        </div>
+      </div>
+
+      <div class="cardUser">
+        <div class="addDocs" v-if="isVisibleAddDoc">
+          <addDocsVue @message-sent-pdf="updateMessagePdf" />
+        </div>
+      </div> -->
+
+<!-- fait -->
+      <!-- <div class="cardUser">
+        <div class="listDocsUser">
+          <listDocsUser
+            v-for="userDocs in listUserDocs"
+            :key="userDocs.id"
+            :userDocs="userDocs"
+            @message-sent="updateMessage"
+          />
+        </div>
+      </div>
+    </div> -->
   </div>
 </template>
 
 <script>
-import pdfViewer from "@/components/PDFViewer"
+import pdfViewer from "@/components/PDFViewer";
 import NavBar from "../components/navBar";
 import infosUserProfil from "../components/infosUserProfil";
 import listDocsUser from "../components/listDocsUser";
 import addDocsVue from "../components/addDocs";
 import contact from "../components/contact";
-
+import "../assets/css/themes.scss";
 
 export default {
-    name: "profil",
-    components: { NavBar, infosUserProfil, listDocsUser, pdfViewer, addDocsVue, contact },
-    data() {
+  name: "profil",
+  components: {
+    NavBar,
+    infosUserProfil,
+    listDocsUser,
+    pdfViewer,
+    addDocsVue,
+    contact,
+  },
+  data() {
     return {
-
-        listUserInfos: [ ],
-        listUserDocs: [ ],
-        message:"erreur",
-        userPDF: [],
-        isVisibleAddDoc: false,
-        isVisibleBtnAddDoc: true,
-        UserlastPDF: "",
-
-
+      listUserInfos: [],
+      listUserDocs: [],
+      message: "erreur",
+      userPDF: [],
+      isVisibleAddDoc: false,
+      isVisibleBtnAddDoc: true,
+      UserlastPDF: "",
     };
   },
-  computed: {
-
-  },
-
+  computed: {},
 
   methods: {
     btnAddDoc() {
-      this.isVisibleAddDoc = true
-        this.isVisibleBtnAddDoc = false
+      this.isVisibleAddDoc = true;
+      this.isVisibleBtnAddDoc = false;
     },
     updateMessagePdf(newValuePDF) {
       console.log(newValuePDF);
       if (newValuePDF == "pdfValide") {
         //this.listUserDocs = []
-        console.log(this.listUserDocs)
-        this.isVisibleAddDoc = false
-        this.isVisibleBtnAddDoc = true
-        fetch('http://127.0.0.1:8080/userDocs'  , {
-          method: 'get',
+        console.log(this.listUserDocs);
+        this.isVisibleAddDoc = false;
+        this.isVisibleBtnAddDoc = true;
+        fetch("http://127.0.0.1:8080/userDocs", {
+          method: "get",
           headers: {
-            'Authorization': `Bearer ${localStorage.getItem('PAC-token')}`
+            Authorization: `Bearer ${localStorage.getItem("PAC-token")}`,
           },
-      })
-      .then(res => res.json())
-      .then(data => {
-        this.listUserDocs = data;
-        // console.log(data)
-        // traitement
-      })
-      .catch(e => {
-        console.error(e);
-      })
-
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            this.listUserDocs = data;
+            // console.log(data)
+            // traitement
+          })
+          .catch((e) => {
+            console.error(e);
+          });
       }
     },
 
@@ -114,100 +181,128 @@ export default {
       console.log(newValue);
       console.log(documentID);
       this.message = newValue;
-      this.userPDF = []
-        if (newValue == "ok") {
-          fetch(`http://127.0.0.1:8080/userPDF/${documentID}` , {
-          method: 'get',
+      this.userPDF = [];
+      if (newValue == "ok") {
+        fetch(`http://127.0.0.1:8080/userPDF/${documentID}`, {
+          method: "get",
           headers: {
-            'Authorization': `Bearer ${localStorage.getItem('PAC-token')}`
+            Authorization: `Bearer ${localStorage.getItem("PAC-token")}`,
           },
-      })
-      .then(res => res.json())
-      .then(data => {
-        this.userPDF = data;
-        console.log("chargement réussi " + this.userPDF)
-        return this.userPDF
-      //   const blobData = new Uint8Array(data);
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            this.userPDF = data;
+            console.log("chargement réussi " + this.userPDF);
+            return this.userPDF;
+            //   const blobData = new Uint8Array(data);
 
-      // // Créer une URL blob à partir du tableau d'octets
-      // const blobUrl = URL.createObjectURL(new Blob([blobData], { type: 'application/pdf' }));
-      //   let reader = new FileReader();
-      //   //reader.readAsDataURL(blobUrl); // convertit le blob en base64 et appelle onload
-      // // Assigner l'URL blob à la propriété documentUrl
+            // // Créer une URL blob à partir du tableau d'octets
+            // const blobUrl = URL.createObjectURL(new Blob([blobData], { type: 'application/pdf' }));
+            //   let reader = new FileReader();
+            //   //reader.readAsDataURL(blobUrl); // convertit le blob en base64 et appelle onload
+            // // Assigner l'URL blob à la propriété documentUrl
 
-      // //L'ajouter au localstorage ?
-      // console.log(blobUrl)
-      // this.userPDF = blobUrl;
-        // traitement
-      })
-       .catch(e => {
-         console.error(e);
-         } )
+            // //L'ajouter au localstorage ?
+            // console.log(blobUrl)
+            // this.userPDF = blobUrl;
+            // traitement
+          })
+          .catch((e) => {
+            console.error(e);
+          });
       }
-
-    }
+    },
   },
 
-    mounted () {
-
-      fetch('http://127.0.0.1:8080/userInfos'  , {
-          method: 'get',
-          headers: {
-            'Authorization': `Bearer ${localStorage.getItem('PAC-token')}`
-          },
-      })
-      .then(res => res.json())
-      .then(data => {
+  mounted() {
+    fetch("http://127.0.0.1:8080/userInfos", {
+      method: "get",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("PAC-token")}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
         this.listUserInfos = data;
         // console.log(data)
         // traitement
       })
-      .catch(e => {
+      .catch((e) => {
         console.error(e);
       }),
-
-
-      fetch('http://127.0.0.1:8080/userDocs'  , {
-          method: 'get',
-          headers: {
-            'Authorization': `Bearer ${localStorage.getItem('PAC-token')}`
-          },
+      fetch("http://127.0.0.1:8080/userDocs", {
+        method: "get",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("PAC-token")}`,
+        },
       })
-      .then(res => res.json())
-      .then(data => {
-        this.listUserDocs = data;
-        // console.log(data)
+        .then((res) => res.json())
+        .then((data) => {
+          this.listUserDocs = data;
+          // console.log(data)
+          // traitement
+        })
+        .catch((e) => {
+          console.error(e);
+        });
+
+    fetch("http://127.0.0.1:8080/userLastDocs", {
+      method: "get",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("PAC-token")}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        this.userPDF = data;
+        //console.log(data)
         // traitement
       })
-      .catch(e => {
+      .catch((e) => {
         console.error(e);
-      })
-
-       fetch('http://127.0.0.1:8080/userLastDocs'  , {
-           method: 'get',
-           headers: {
-             'Authorization': `Bearer ${localStorage.getItem('PAC-token')}`
-           },
-       })
-       .then(res => res.json())
-       .then(data => {
-         this.userPDF = data;
-         //console.log(data)
-         // traitement
-       })
-       .catch(e => {
-         console.error(e);
-       })
-
-
-
-    }
-
-}
+      });
+  },
+};
 </script>
 
 
 <style>
+.grid {
+  display: grid;
+  max-width: 100%;
+  grid-template-columns: 1fr 1fr; /* 2 colonnes de largeur égale */
+  grid-gap: 2%
+}
+
+.left-column,
+.right-column {
+  padding: 10px;
+  background-color: #837b7b;
+}
+
+.row-1 {
+  height: 45%;
+  padding: 10px;
+  background-color: #347d9e;
+}
+
+.row-2 {
+  padding: 10px;
+  background-color: #bebc1e;
+}
+
+.grid-row-2 {
+  display: grid;
+  grid-template-columns: repeat(2, auto); /* 2 colonnes de largeur égale */
+  grid-gap: 10px; /* Espacement entre les éléments de la grille */
+}
+
+.grid-row-2-item {
+  justify-self: center;
+  background: orange;
+}
+
+/*_____________________________________________*/
 
 .image {
   display: none;
@@ -229,8 +324,6 @@ export default {
   padding: 0 3px;
 }
 
-
-
 b-form-input {
   margin-bottom: 10px;
 }
@@ -245,14 +338,14 @@ b-form-input {
   margin-top: 20px;
 }
 
-#webViewer{
-  height: 100vh;
-  width: 50vw;
+#webViewer {
+  height: 100%;
+  width: 50%;
 }
 
 .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
@@ -277,12 +370,10 @@ b-form-input {
   grid-template-columns: repeat(2, auto);
   grid-gap: 10px;
   grid-auto-rows: minmax(300px, auto);
-
 }
 
 .cardUser {
   height: 350px;
-
 }
 
 /*
@@ -297,17 +388,11 @@ b-form-input {
   margin-left: 20px;
   grid-column: 1/2;
   grid-row: 2;
-
-
 }
 
-.infosUserProfil{
+.infosUserProfil {
   margin-left: 20px;
   grid-column: 1 / 2;
   grid-row: 1;
-
 }
-
-
-
 </style>
