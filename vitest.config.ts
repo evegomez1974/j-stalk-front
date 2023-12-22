332b71d6b3d528ef73705130dcfd71616780773b
Eve.gomez <eve.gomez@campus.academy>
2023-12-22 15:02:00 +0100

    test configuration vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   test: {
-    reporters: ['junit']
+    reporters: ['junit'],
+    outputFile: 'testsReports/test-output.xml'
   },
 })
