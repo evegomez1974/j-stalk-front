4ce0d09449f910fcd9d0863b4f2b7c50b8e86c7f
Eve.gomez <eve.gomez@campus.academy>
2024-03-01 14:24:57 +0100

    ci: add version prod j-stalk_front

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,7 +28,6 @@ export default {
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
-  buildDir: 'j-stalk_front_prod',
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
